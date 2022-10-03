845d16f7be9dba8d4eda94fb595e3edee095c6af
Ykyki4 <jimmypantalones@gmail.com>
2022-10-03 17:37:33 +0300

    Change description CharField to TextField

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,7 +5,7 @@ class Pokemon(models.Model):
     title_ru = models.CharField(verbose_name='Имя на русском', max_length=50)
     title_en = models.CharField(verbose_name='Имя на английском', max_length=50, blank=True)
     title_jp = models.CharField(verbose_name='Имя на японском', max_length=50, blank=True)
-    description = models.CharField(verbose_name='Описание покемона', max_length=1000, blank=True)
+    description = models.TextField(verbose_name='Описание покемона', blank=True)
     image = models.ImageField(verbose_name='Изображение покемона', upload_to='images/')
 
     previous_evo = models.ForeignKey(
